aeebabcaecdf0c994aee8f8f3bda2c8849e72155
joaorodrigo <jrodrigo0592@gmail.com>
2022-05-23 16:18:49 -0300

    Projeto finalizado com sucesso

---
+++ RULES.md
@@ -0,0 +1,27 @@
+### Desafio 01 da Trilha NodeJS - TODOS
+
+---
+
+### Requesitos
+
+- [DONE] deve ser possível criar um novo usuário.
+- [DONE] deve ser possível listar todos os TODOs de um usuário.
+- [DONE] deve ser possível listar os detalhes da tarefa de um usuário.
+    OBS: - [X] A data deve ser exibida no formato ANO-MÊS-DIA.
+
+- [DONE] deve ser possível alterar título e prazo de uma tarefa.
+
+- [DONE] deve ser possível alterar a propriedade done para true.
+
+- [NOT-COMPLETED] deve ser possível deletar uma tarefa.
+
+
+---
+
+### Regras de negócio
+
+- [DONE] Não é possível criar um usuário já existente.
+- [DONE] Não é possível atualizar uma tarefa inexistente.
+- [DONE] Não é possível alterar a propriedade done de uma tarefa inexistente.
+- [NOT-COMPLETED] Não é possível deletar uma tarefa inexistente.
+
--- src/index.js
+++ src/index.js
@@ -1,41 +1,124 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
-
 const app = express();
+const users = [];
+const { v4: uuidv4 } = require('uuid')
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
-
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users
+   .find(user => user.username == username)
+
+  if(!user) {
+    return response
+     .status(404)
+     .json({error: "User not found!"})
+  }
+
+  request.user = user;
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userExists = users.find(user =>
+    user.username == username
+  );
+
+  if(userExists) {
+    return response
+     .status(400)
+     .json({error: "Username already exists!"});
+  }
+
+  const user = {id: uuidv4(), name, username, todos: []}
+  
+  users.push(user);
+
+  return response
+    .status(201)
+    .json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const newTodos = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date("2022-05-23")}
+
+  user
+   .todos
+   .push(newTodos)
+
+  return response
+   .status(201)
+   .json(newTodos)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id == id);
+
+  if(!todo) {
+    return response.status(404).json({error: "You are trying to update a non-existent TODO "})
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(200).json(todo);
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params
+
+  const todo = user.todos.find(todo => todo.id == id);
+
+  if(!todo) {
+    return response.status(404).json({error: "You are trying to update a non-existent TODO"});
+  }
+
+  todo.done = true;
+
+  return response.status(200).json(todo);
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  
+  const todo = user.todos.find(todo => todo.id == id);
+
+  if(!todo) {
+    return response.status(404).json({error: "You are trying to delete a non-existent TODO"});
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).json(todo);
 });
 
 module.exports = app;
